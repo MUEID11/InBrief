617570ae0a22697ccaffec35a96a6cbee0a80bd4
MUEID11 <mehedirangpur3@gmail.com>
2024-09-20 16:21:45 +0600

    added logos and favicon

--- src/Components/Component/Navbar.jsx
+++ src/Components/Component/Navbar.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { FaRegPenToSquare } from 'react-icons/fa6';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import MyImageSvg from '../../assets/digital-news.gif';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { FaRegPenToSquare } from "react-icons/fa6";
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "./../../assets/logo.png"
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => {
@@ -10,39 +10,45 @@ const Navbar = () => {
   };
   const navLinks = (
     <>
-      <Link to="/" className="hover:text-gray-400">
+      <Link to="/" className="hover:text-gray-700">
         Home
       </Link>
-      <Link to="/stories" className="hover:text-gray-400">
+      <Link to="/stories" className="hover:text-gray-700">
         Stories
       </Link>
-      <Link to="/about" className="hover:text-gray-400">
+      <Link to="/about" className="hover:text-gray-700">
         About
       </Link>
-      <Link to="/signup" className="hover:text-gray-400">
+      <Link to="/signup" className="hover:text-gray-700">
         Sign Up
       </Link>
     </>
   );
   return (
-    <nav className="bg-white  text-violet-800 px-4 sm:px-10">
+    <nav className="bg-white  text-red-500 px-4 sm:px-10">
       <div className="container mx-auto flex justify-between items-center py-3 sm:py-5">
         {/* Logo */}
-        <div className="flex text-sm  sm:text-2xl  font-bold bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 bg-clip-text text-transparent">
-          <img src={MyImageSvg} alt=" Image Svg" className="w-[25px]  rounded-lg " />
-
-          <a href="/">
-            In<span className="text-2xl">Brief</span>
-          </a>
+        <div className="flex text-sm  sm:text-2xl  font-bold">
+          <img
+            src={logo}
+            alt=" Image Svg"
+            className="w-16 sm:w-44"
+          />
         </div>
 
         {/* Hamburger Icon */}
         <div className="sm:hidden">
-          <button onClick={toggleMenu}>{isOpen ? <FaTimes className="text-2xl" /> : <FaBars className="text-2xl" />}</button>
+          <button onClick={toggleMenu}>
+            {isOpen ? (
+              <FaTimes className="text-2xl" />
+            ) : (
+              <FaBars className="text-2xl" />
+            )}
+          </button>
         </div>
 
         {/* Links */}
-        <div className="hidden  sm:flex space-x-3 sm:space-x-5 mr-0 sm:mr-40  bg-gradient-to-r from-purple-800 via-pink-700 to-blue-800 bg-clip-text text-transparent">
+        <div className="hidden  sm:flex space-x-3 sm:space-x-5 mr-0 sm:mr-40 text-red-500 font-medium">
           {navLinks}
         </div>
 
@@ -53,19 +59,27 @@ const Navbar = () => {
         </div>
 
         <div className="hidden sm:flex">
-          <input type="text" placeholder="Search..." className="px-4 py-2 rounded-md text-gray-700 focus:outline-none border border-gray-300" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="px-4 py-2 rounded-md text-gray-700 focus:outline-none border border-gray-300"
+          />
         </div>
       </div>
 
       {/* Mobile Menu      === (Shown when the hamburger icon is clicked) */}
-      <div className={`sm:hidden ${isOpen ? 'block' : 'hidden'}`}>
+      <div className={`sm:hidden ${isOpen ? "block" : "hidden"}`}>
         <div className="px-4 pb-4 space-y-2">
           {navLinks}
           <div className="flex items-center py-2">
             <FaRegPenToSquare className=" text-xl" />
             <h2 className="ml-2">Write</h2>
           </div>
-          <input type="text" placeholder="Search..." className="w-full px-4 py-2 rounded-md text-gray-700 focus:outline-none border border-gray-300" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full px-4 py-2 rounded-md text-gray-700 focus:outline-none border border-gray-300"
+          />
         </div>
       </div>
     </nav>
