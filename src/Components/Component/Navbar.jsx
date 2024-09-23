63f1d05d9ddca5bfd7d804ca5743f14c32112ffc
Jisun696 <istiakkashem35@gmail.com>
2024-09-23 10:37:45 +0600

    make the nav responsive for tablet devices and othe ui edits

--- src/Components/Component/Navbar.jsx
+++ src/Components/Component/Navbar.jsx
@@ -10,56 +10,63 @@ const Navbar = () => {
   };
   const navLinks = (
     <>
-      <Link to="/" className="hover:text-gray-700">
+      <Link to="/" className="hover:text-neutral-700 transition-all duration-500">
         Home
       </Link>
-      <Link to="/stories" className="hover:text-gray-700">
+      <Link to="/stories" className="hover:text-neutral-700 transition-all duration-500">
         Stories
       </Link>
-      <Link to="/about" className="hover:text-gray-700">
+      <Link to="/about" className="hover:text-neutral-700 transition-all duration-500">
         About
       </Link>
-      <Link to="/signup" className="hover:text-gray-700">
+      <Link to="/signup" className="hover:text-neutral-700 transition-all duration-500">
         Sign Up
       </Link>
     </>
   );
   return (
-    <nav className="bg-white  text-red-500 px-4 sm:px-10">
+    <nav className="bg-white  text-red-500 px-4 lg:px-10">
       <div className="container mx-auto flex justify-between items-center py-3 sm:py-5">
         {/* Logo */}
-        <div className="flex text-sm  sm:text-2xl  font-bold">
+        <div>
           <img src={logo} alt=" Image Svg" className="w-28 sm:w-44" />
         </div>
 
         {/* Hamburger Icon */}
-        <div className="sm:hidden">
+        {/* <div className="sm:hidden">
           <button onClick={toggleMenu}>{isOpen ? <FaTimes className="text-2xl" /> : <FaBars className="text-2xl" />}</button>
-        </div>
+        </div> */}
 
         {/* Links */}
-        <div className="hidden sm:flex space-x-3 sm:space-x-5 mr-0 sm:mr-40 text-red-500 font-medium">{navLinks}</div>
+        <div className="hidden lg:flex space-x-3 sm:space-x-5 mr-0  text-red-600 text-lg font-bold uppercase">{navLinks}</div>
 
         {/* Write Icon and Search Input */}
-        <div className="hidden sm:flex items-center">
-          <FaRegPenToSquare className="text-lg sm:text-xl" />
-          <h2 className="ml-2">Write</h2>
-        </div>
+        <div className="flex gap-2 items-center justify-center">
+          <div className="hidden sm:flex items-center">
+            <FaRegPenToSquare className="text-lg sm:text-xl" />
+            <h2 className="lg:mr-4 text-red-600 font-medium text-lg">Write</h2>
+          </div>
 
-        <div className="hidden sm:flex">
-          <input type="text" placeholder="Search..." className="px-4 py-2 rounded-md text-gray-700 focus:outline-none border border-gray-300" />
+          <div className="hidden sm:flex">
+            <input type="text" placeholder="Search..." className="px-4 py-2 rounded-md text-gray-700  focus:ring-rose-300 focus:ring focus:outline-none border border-rose-300" />
+          </div>
+
+          {/* Hamburger Icon (for tablet view) */}
+          <div className="lg:hidden">
+            <button onClick={toggleMenu}>{isOpen ? <FaTimes className="text-2xl" /> : <FaBars className="text-2xl" />}</button>
+          </div>
         </div>
       </div>
 
       {/* Mobile Menu      === (Shown when the hamburger icon is clicked) */}
-      <div className={`sm:hidden ${isOpen ? 'block' : 'hidden'}`}>
+      <div className={`lg:hidden ${isOpen ? 'block' : 'hidden'}`}>
         <div className="px-4 pb-4 space-y-2 flex flex-col">
-          {navLinks}
-          <div className="flex items-center py-2">
+          <div className="pb-4 space-y-3 flex flex-col font-bold uppercase">{navLinks}</div>
+          <div className="flex items-center py-2 sm:hidden">
             <FaRegPenToSquare className=" text-xl" />
             <h2 className="ml-2">Write</h2>
           </div>
-          <input type="text" placeholder="Search..." className="w-full px-4 py-2 rounded-md text-gray-700 focus:outline-none border border-gray-300" />
+          <input type="text" placeholder="Search..." className="w-full px-4 py-2 rounded-md text-gray-700 focus:outline-none border border-gray-300 sm:hidden" />
         </div>
       </div>
     </nav>
