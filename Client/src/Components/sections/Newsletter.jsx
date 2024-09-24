2acb735a6d6c2718187d4d590a0376b6fc339d6c
MUEID11 <mehedirangpur3@gmail.com>
2024-09-25 00:16:09 +0600

    minor ui test-client

--- Client/src/Components/sections/Newsletter.jsx
+++ Client/src/Components/sections/Newsletter.jsx
@@ -9,13 +9,13 @@ const Newsletter = () => {
             <span className="text-rose-600">subscribe</span>✍️ our latest updates
           </h3>
         </div>
-        <div className="w-full text-center md:w-1/2 ">
+        <div className="w-full text-center md:w-1/2 space-y-3 sm:space-y-0">
           <input
             type="text"
-            className="text-lg bg-white text-black sm:w-72 pl-5 placeholder-neutral-500 py-3 rounded-lg focus:ring-rose-300 focus:ring focus:outline-none"
+            className="text-lg bg-white text-black sm:w-72 pl-5 placeholder-neutral-500 py-3 focus:ring-rose-200 focus:ring focus:outline-none border-red-500 border border-r-4 border-b-4"
             placeholder="Enter Your Email"
           />
-          <button className="text-lg bg-red-600 text-white px-4 py-3 rounded-lg ml-1 font-inter"> subscribe</button>
+          <button className="text-lg bg-red-600 text-white sm:px-4 sm:py-3 px-2 py-2  rounded-lg ml-1 font-inter"> subscribe</button>
         </div>
       </div>
     </section>
