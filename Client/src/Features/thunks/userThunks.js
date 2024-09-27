ef92b28d1f7bc15b58b7f31998920accbe12f21f
MUEID11 <mehedirangpur3@gmail.com>
2024-09-27 12:52:31 +0600

    sign in fixed

--- Client/src/Features/thunks/userThunks.js
+++ Client/src/Features/thunks/userThunks.js
@@ -2,11 +2,13 @@ import {createAsyncThunk} from "@reduxjs/toolkit"
 
 
 const userThunk = createAsyncThunk('user/userThunk', async() => {
-  console.log(1)
+  
+  const getToken = localStorage.getItem('token')
+  console.log('thunk',getToken)
   try {
     const response = await fetch(`${import.meta.env.VITE_API_URL}/users/uservalidation`, {
       method: "GET",
-      headers: {'content-type': "application/json", "Authorization": `Bearer ${localStorage.getItem('token')}`}
+      headers: {'content-type': "application/json", "Authorization": `Bearer ${getToken}`}
     })
     const data = await response.json();
     console.log('conosling from user thank',data);
