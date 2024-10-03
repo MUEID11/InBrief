9f36e1cfff0973e9fef318a474f580c9685e1995
Sumaiya Urmi <akthersumaiya689@gmail.com>
2024-10-03 21:35:48 +0600

    ui responsive - client

--- Client/src/Components/sections/Newsletter.jsx
+++ Client/src/Components/sections/Newsletter.jsx
@@ -2,18 +2,18 @@ const Newsletter = () => {
   return (
     <section className="sm:m-0 m-2 mb-12">
       <div className="border border-red-600 border-r-4 border-b-4 container mx-auto sm:p-10 px-4 sm:py-16 max-sm:py-8 mt-12 flex flex-col md:flex-row items-center justify-between gap-4 ">
-        <div className="w-full md:w-1/2 ">
+        <div className="w-full md:w-1/2 text-center">
           <p className="font-bold max-sm:text-sm text-gray-600 uppercase tracking-[0.3em]">
             {" "}
             GET FIRST UPDATE
           </p>
-          <h3 className="font-bold text-3xl max-sm:text-xl mt-2 font-inter ">
+          <h3 className="font-bold text-lg md:text-xl lg:text-2xl mt-2 font-inter ">
             Get the news infront line by <br />
             <span className="text-rose-600">subscribe</span>✍️ our latest
             updates
           </h3>
         </div>
-        <div className="w-full text-center md:w-1/2 space-y-3 sm:space-y-0">
+        <div className="w-full text-center md:w-1/2 space-y-3 sm:space-y-0 flex justify-center items-center">
           <input
             type="text"
             className="text-lg bg-white text-black sm:w-72 pl-5 placeholder-neutral-500 py-3 focus:ring-rose-200 focus:ring focus:outline-none border-red-500 border border-r-4 border-b-4"
