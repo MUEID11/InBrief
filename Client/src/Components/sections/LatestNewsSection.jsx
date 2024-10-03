345372837fb24e79074114fb8844aea70d8105e8
Jisun696 <istiakkashem35@gmail.com>
2024-10-03 18:34:18 +0600

    responsive edits and limit data fetching according to viewport

--- Client/src/Components/sections/LatestNewsSection.jsx
+++ Client/src/Components/sections/LatestNewsSection.jsx
@@ -1,19 +1,31 @@
+/* eslint-disable no-unused-vars */
 // src/Components/LatestNewsSection.jsx
 
-import { useEffect, useState } from "react";
-import NewsSection from "./NewsSection";
+import { useEffect, useState } from 'react';
+import NewsSection from './NewsSection';
 
 const LatestNewsSection = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
+  const [width, setWidth] = useState(window.innerWidth);
+  console.log(width);
+  let url;
+  if (width <= 1536 && width >= 1280) {
+    url = `http://localhost:5000/articles?sort=dsc&limit=3`;
+  } else if (width >= 1024 && width <= 1280) {
+    url = `http://localhost:5000/articles?sort=dsc&limit=2`;
+  } else {
+    url = `http://localhost:5000/articles?sort=dsc&limit=4`;
+  }
+
   // Fetch Latest News data from fakedata.json
   useEffect(() => {
-    fetch("/fakedata.json")
+    fetch(url)
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok');
         }
         return response.json();
       })
@@ -22,11 +34,11 @@ const LatestNewsSection = () => {
         setLoading(false);
       })
       .catch((err) => {
-        console.error("Error fetching Latest News:", err);
+        console.error('Error fetching Latest News:', err);
         setError(true);
         setLoading(false);
       });
-  }, []);
+  }, [url]);
 
   if (loading) {
     return (
@@ -39,20 +51,14 @@ const LatestNewsSection = () => {
   if (error) {
     return (
       <div className="flex items-center justify-center p-4">
-        <p className="text-xl text-red-600">
-          Failed to load Latest News. Please try again later.
-        </p>
+        <p className="text-xl text-red-600">Failed to load Latest News. Please try again later.</p>
       </div>
     );
   }
 
   return (
     <div className="container mx-auto m-2">
-      <NewsSection
-        title="Latest News"
-        articles={articles}
-        link="/latest-news"
-      />
+      <NewsSection title="Latest News" articles={articles} link="/latest-news" />
     </div>
   );
 };
