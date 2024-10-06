786f0579c4d7867985051742c92933b0b84fbf5d
Jisun696 <istiakkashem35@gmail.com>
2024-10-07 00:06:56 +0600

    conflicts resolved

--- Client/src/Components/Component/NewsCard.jsx
+++ Client/src/Components/Component/NewsCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import { LuArrowBigUpDash } from 'react-icons/lu';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { IoBookmarksOutline, IoBookmarksSharp } from 'react-icons/io5';
 import toast from 'react-hot-toast';
 import { useAddBookmarkMutation } from '../../services/bookmarksApi';
@@ -91,40 +91,42 @@ const NewsCard = ({ article }) => {
 
   return (
     <article className="shadow-lg p-5 border border-red-600 border-r-4 border-b-4 flex flex-col transition-all duration-300 ease-in-out hover:border-gray-600 hover:scale-105 h-full rounded-sm">
-      {/* Link wrapping Image */}
-      <a href={article.url} target="_blank" rel="noopener noreferrer" aria-label={`Read more about ${article.title}`}>
-        <img src={article.image} alt={article.title} className="h-56 object-cover w-full" loading="lazy" />
-      </a>
-
-      {/* Info Section */}
-      <div className="mt-4 flex flex-col flex-grow">
-        {/* Source Section */}
-        <div className="flex gap-2 items-center">
-          {/* <img
-            src="https://via.placeholder.com/20" // Placeholder for the source icon
-            className="size-5 bg-red-700 rounded-full object-cover"
-            alt={`${article?.source?.url}`}
-          /> */}
-          <div className="size-2 bg-red-700 rounded-full"></div>
-          <span className="text-sm text-gray-600">{article?.source?.name || article.source}</span>
-        </div>
-
-        {/* Headline */}
-        <a href={article.url} target="_blank" rel="noopener noreferrer">
-          <h3 className="font-bold text-lg mt-2">{article?.title}</h3>
+      <Link to={`articles/${article?._id}`}>
+        {/* Link wrapping Image */}
+        <a href={article.url} target="_blank" rel="noopener noreferrer" aria-label={`Read more about ${article.title}`}>
+          <img src={article.image} alt={article.title} className="h-56 object-cover w-full" loading="lazy" />
         </a>
-        {/* Date and Category */}
-        <div className="flex gap-3 items-center my-2">
-          <p className="text-red-600 font-semibold">{article?.region}</p>
-          <span className="text-xs">{new Date(article.createdAt).toLocaleDateString()}</span>
+
+        {/* Info Section */}
+        <div className="mt-4 flex flex-col flex-grow">
+          {/* Source Section */}
+          <div className="flex gap-2 items-center">
+            {/* <img
+                src="https://via.placeholder.com/20" // Placeholder for the source icon
+                className="size-5 bg-red-700 rounded-full object-cover"
+                alt={`${article?.source?.url}`}
+              /> */}
+            <div className="size-2 bg-red-700 rounded-full"></div>
+            <span className="text-sm text-gray-600">{article?.source?.name || article.source}</span>
+          </div>
+
+          {/* Headline */}
+          <a href={article.url} target="_blank" rel="noopener noreferrer">
+            <h3 className="font-bold text-lg mt-2">{article?.title}</h3>
+          </a>
+          {/* Date and Category */}
+          <div className="flex gap-3 items-center my-2">
+            <p className="text-red-600 font-semibold">{article?.region}</p>
+            <span className="text-xs">{new Date(article.createdAt).toLocaleDateString()}</span>
+          </div>
+          {/* Description */}
+          <p className="text-sm text-gray-600 mb-4 flex-grow">{`${article.description.substring(0, 100)}...`}</p>
         </div>
-        {/* Description */}
-        <p className="text-sm text-gray-600 mb-4 flex-grow">{`${article.description.substring(0, 100)}...`}</p>
-      </div>
-      <div className="flex gap-3 items-center justify-between mb-2">
-        <p className="text-red-600 font-semibold">{article?.category ? article?.category : 'Category'}</p>
-        <span className="text-xs">{article?.date}</span>
-      </div>
+        <div className="flex gap-3 items-center justify-between mb-2">
+          <p className="text-red-600 font-semibold">{article?.category ? article?.category : 'Category'}</p>
+          <span className="text-xs">{article?.date}</span>
+        </div>
+      </Link>
 
       <div className="flex justify-between items-center">
         <div>
