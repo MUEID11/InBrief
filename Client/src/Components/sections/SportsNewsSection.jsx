0fd600e3baf80a30290f776fee8e616dc97ac8ba
Jisun696 <istiakkashem35@gmail.com>
2024-10-10 13:06:55 +0600

    bug fixes

--- Client/src/Components/sections/SportsNewsSection.jsx
+++ Client/src/Components/sections/SportsNewsSection.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
-import NewsSection from "./NewsSection";
+import { useEffect, useState } from 'react';
+import NewsSection from './NewsSection';
 
-const SportsNewsSection = () => {
+const SportsNewsSection = ({ isHomeSection = false }) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
@@ -10,12 +10,18 @@ const SportsNewsSection = () => {
   const [width, setWidth] = useState(window.innerWidth);
   console.log(width);
   let url;
-  if (width <= 1536 && width >= 1280) {
-    url = `${import.meta.env.VITE_API_URL}/articles?category=sports&limit=3`;
-  } else if (width >= 1024 && width <= 1280) {
-    url = `${import.meta.env.VITE_API_URL}/articles?category=sports&limit=2`;
+  // let url = `${import.meta.env.VITE_API_URL}/articles?category=sports`;
+
+  if (isHomeSection) {
+    if (width <= 1536 && width >= 1280) {
+      url = `${import.meta.env.VITE_API_URL}/articles?category=sports&limit=3`;
+    } else if (width >= 1024 && width <= 1280) {
+      url = `${import.meta.env.VITE_API_URL}/articles?category=sports&limit=2`;
+    } else {
+      url = `${import.meta.env.VITE_API_URL}/articles?category=sports&limit=4`;
+    }
   } else {
-    url = `${import.meta.env.VITE_API_URL}/articles?category=sports&limit=4`;
+    url = `${import.meta.env.VITE_API_URL}/articles?category=sports`;
   }
 
   // Fetch Business News data from API
@@ -23,7 +29,7 @@ const SportsNewsSection = () => {
     fetch(url)
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok');
         }
         return response.json();
       })
@@ -32,7 +38,7 @@ const SportsNewsSection = () => {
         setLoading(false);
       })
       .catch((err) => {
-        console.error("Error fetching Business News:", err);
+        console.error('Error fetching Business News:', err);
         setError(true);
         setLoading(false);
       });
@@ -49,9 +55,7 @@ const SportsNewsSection = () => {
   if (error) {
     return (
       <div className="flex items-center justify-center p-4">
-        <p className="text-xl text-red-600">
-          Failed to load Sports News. Please try again later.
-        </p>
+        <p className="text-xl text-red-600">Failed to load Sports News. Please try again later.</p>
       </div>
     );
   }
