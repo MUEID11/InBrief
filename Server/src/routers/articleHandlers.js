7a82982c4e737bee4c2b222166c84557c3b65f5c
Md Ziad <mdujanpuc@gamil.com>
2024-10-18 03:17:38 +0600

    admin approval

--- Server/src/routers/articleHandlers.js
+++ Server/src/routers/articleHandlers.js
@@ -1,3 +1,4 @@
+// Inside articleHandlers.js
 const express = require("express");
 const {
   getArticles,
@@ -9,8 +10,13 @@ const {
   getArticlesByEmail,
   deleteArticle,
   getArticlesByPreferences,
+  updateStatus,
 } = require("../controllers/ArticleControllers/article.controller");
 const { searchByCategory } = require("../controllers/searchController");
+
+// Import the updateStatus controller function
+// const { updateStatus } = require("../controllers/ArticleControllers/article.controller");
+
 const router = express.Router();
 
 router.get("/", getArticles);
@@ -20,7 +26,9 @@ router.patch("/addBookmark", addToBookmark);
 router.get("/allBookmarks", getAllBookmarks);
 router.get("/search", searchByCategory);
 router.patch("/addLike", AddLike);
+router.patch("/updateStatus/:id", updateStatus); // Add the new updateStatus route
 router.get("/:id", getArticleById); // Get a specific article by ID
 router.get("/user/:email", getArticlesByEmail);
 router.delete("/:id", deleteArticle);
+
 module.exports = router;
