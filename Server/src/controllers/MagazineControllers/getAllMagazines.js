ac8331cf7f6d7cd037b5955b15c7b2a1e2095eee
Jisun696 <istiakkashem35@gmail.com>
2024-10-24 10:50:39 +0600

    edit the getAllMagazines api

--- Server/src/controllers/MagazineControllers/getAllMagazines.js
+++ Server/src/controllers/MagazineControllers/getAllMagazines.js
@@ -2,8 +2,14 @@ const Magazine = require("../../models/Magazine/magazineModel");
 
 const getAllMagazines = async (req, res) => {
   try {
+    const { creatorId } = req?.query;
+
+    let query = {};
+    if (creatorId) {
+      query = { creator: creatorId };
+    }
     // Retrieve all magazines from the database
-    const magazines = await Magazine.find().populate("followers", "name").populate("articles", "title").populate("collaborators", "name");
+    const magazines = await Magazine.find(query).populate("followers", "name").populate("articles", "title").populate("collaborators", "name");
 
     res.status(200).json(magazines); // Return magazines in the response
   } catch (error) {
